bc3060cfd8ae012e13405a22e9c40994387a414f
syaukaniakbar <syaukaniakbar2019@gmail.com>
2025-09-24 14:57:50 +0800

    api for get document in accordance the category

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,26 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegionController;
 use App\Http\Controllers\SiswaController;
+use App\Models\Document;
 
 Route::get('/regions/provinces', [RegionController::class, 'getProvinces']);
-Route::get('/regions/cities/{provinceId}', [RegionController::class, 'getCities']);
-Route::get('/regions/districts/{cityId}', [RegionController::class, 'getDistricts']);
-Route::get('/regions/villages/{districtId}', [RegionController::class, 'getVillages']);
+Route::get('/regions/cities/{provinceCode}', [RegionController::class, 'getCities']);
+Route::get('/regions/districts/{cityCode}', [RegionController::class, 'getDistricts']);
+Route::get('/regions/villages/{districtCode}', [RegionController::class, 'getVillages']);
 
 Route::get('/siswa/check-nisn/{nisn}', [SiswaController::class, 'checkNisn']);
+Route::get('/siswa/get-by-nisn/{nisn}', [SiswaController::class, 'getByNisn']);
+
+// Document routes
+Route::get('/documents/category/{category}', function ($category) {
+    $document = Document::where('category', $category)->first();
+    
+    if ($document) {
+        // Add the full URL to the document, properly encoding the file path
+        $filePath = str_replace(' ', '%20', $document->file_path);
+        $document->file_url = asset('storage/' . $filePath);
+        return response()->json($document);
+    }
+    
+    return response()->json(['message' => 'Document not found'], 404);
+});
